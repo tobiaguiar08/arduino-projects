0972c2fead777bc9892494c22f2982a6b65e10fa
Tobias Aguiar <tobias.aguiar-silva-de-almeida@ecole.ensicaen.fr>
2023-04-10 20:05:35 +0200

    USART init
    Signed-off-by: Tobias Aguiar <tobias.aguiar-silva-de-almeida@ecole.ensicaen.fr>

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,8 +1,23 @@
+#define F_CPU 16000000UL
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define BAUD 9600
+#define UBRR_VAL ((F_CPU/16/BAUD) - 1)
+
+void uart_init() {
+  UBRR0H = (unsigned char)(UBRR_VAL >> 8);
+  UBRR0L = (unsigned char)UBRR_VAL;
+  UCSR0B = (1 << TXEN0);
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
 
 int main ()
 {
   /*Init*/
-  while(1);
+  uart_init();
+  while (1);
   return 0;
 }
